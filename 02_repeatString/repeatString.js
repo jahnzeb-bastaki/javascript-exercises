d13796c95310e660eeaeb52d8b8938330bd574b4
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2024-05-17 16:54:23 -0500

    Finished Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeatAmount) {
+  newString = ''
+  if(repeatAmount < 0){
+    return "ERROR"
+  } else if(repeatAmount == 0){
+    return ""
+  } else {
+    for(let i = 0; i < repeatAmount; i++){
+      newString += str
+    }
+    return newString
+  }
 };
 
 // Do not edit below this line
