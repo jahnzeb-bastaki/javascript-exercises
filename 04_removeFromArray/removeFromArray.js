06fd87ea196a090fd084fc0027b84340e3108732
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2024-05-17 17:11:09 -0500

    Finished Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+  let newArray = []
+  array.forEach(element => {
+    if(!items.includes(element)){
+      newArray.push(element)
+    }
+  })
+  return newArray
 };
 
 // Do not edit below this line
