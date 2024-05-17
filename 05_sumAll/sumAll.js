9a24e139c54eefb26db5ec0ecc1c5dd178d28c1e
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2024-05-17 17:38:44 -0500

    Finished Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let lowerBound = Math.min(num1, num2)
+  let upperBound = Math.max(num1, num2)
+  if(lowerBound < 0 || upperBound < 0){
+    return 'ERROR'
+  } else if(typeof num1 == "string" || typeof num2 == "string"){
+    return 'ERROR'
+  } else if(typeof num1 == "number" && typeof num2 == "number"){
+    let sum = 0
+    for(let i = lowerBound; i <= upperBound; i++){
+      sum += i
+    }
+    return sum
+  }
+  return 'ERROR'
 };
 
 // Do not edit below this line
